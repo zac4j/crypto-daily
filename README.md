42cf28b06734fd659fa6ffa6a18dfd31d9c86da7
Zac <zac_ju@163.com>
2024-02-03 21:02:44 +0800

    Add API call and init database

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# crypto-daily-contest
-Cryptocurrency Daily Contest
+# Cryptocurrency Daily Contest
+
+To run the Flask application, go to the terminal and execute:  `flask run`. The application should be accessible via this url: http://127.0.0.1:5000/. 
+
+The first file would be a text file that list the modules our application depends on. To do so execute:
+
+``` bash
+pip freeze > requirements.txt
+```
--- src/app.py
+++ src/app.py
@@ -1,19 +1,63 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+import requests
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Crypto.sqlite3'
 
-@app.route("/")
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+API_ENDPOINT = 'https://api.coingecko.com/api/v3'
+API_GET_COIN_PRICE = '/simple/price'
+API_GET_COIN_MARKET = '/coins/market'
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+TOP_COIN_IDS = 'bitcoin,ethereum,litecoin,bitcoin-cash,binancecoin,eos,ripple,stellar,chainlink,polkadot'
+
+DEFAULT_CURRENCY = "usd"
+
+'''
+TODO
+1.Fetching data from an external source such as a REST API.
+2.Storing the data in a database.
+'''
+
+'''
+Define the database model
+that is used to store 
+the Top Coins.
+'''
+
+db = SQLAlchemy(app)
+class TopCoins(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    coinId = db.Column(db.String(20))
+    price = db.Column(db.Numeric(20))
+
+    def __repr__(self) -> str:
+        return f"CoinMarket(id={self.id!r},coinId={self.coinId!r}, name={self.name!r}, symbol={self.symbol!r}, price={self.price!r})"
+    
+'''
+Helper function to get top10 coins price
+using API https://www.coingecko.com/api/documentation
+'''
+
+
+def get_coins_price(url: str, ids: str):
+    response = requests.get(url, params = {"ids":ids, "vs_currencies":DEFAULT_CURRENCY})
+    return response.json()
+
+
+'''
+In main we first get the current top coins price and then 
+create a new object that we can add to the database. 
+'''
+if __name__ == "__main__":
+    url = API_ENDPOINT + API_GET_COIN_PRICE
+    coins = get_coins_price(url=url, ids=TOP_COIN_IDS)
+    print(f"get coins: {coins}")
+    for coin in coins:
+        price = coins[coin]['usd']
+        new_entry = TopCoins(coinId = coin["id"], price = price)
+        print(f"create new entry: {coin}, value: ${price}")
+        db.session.add(new_entry)
+    db.session.commit
