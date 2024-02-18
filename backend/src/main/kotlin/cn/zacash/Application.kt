d44d590d1024205e5b500cb902c17af26ec3a5d1
Zac <zac_ju@163.com>
2024-02-18 21:36:27 +0800

    update docker files

--- backend/src/main/kotlin/cn/zacash/Application.kt
+++ backend/src/main/kotlin/cn/zacash/Application.kt
@@ -7,19 +7,33 @@ import cn.zacash.plugins.configureRouting
 import cn.zacash.plugins.configureSerialization
 import cn.zacash.repository.CoinRepository
 import cn.zacash.service.CoinService
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.koin.core.parameter.parametersOf
 import org.koin.java.KoinJavaComponent.inject
 import org.litote.kmongo.coroutine.CoroutineClient
+import java.util.Arrays.asList
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
-val coroutineClient: CoroutineClient by inject(CoroutineClient::class.java)
+val coroutineClient: CoroutineClient by inject(CoroutineClient::class.java) {
+    val mongoHost = System.getenv("MONGO_HOST") ?: "mongodb"
+    val mongoPort = (System.getenv("MONGO_PORT") ?: "27017").toInt()
+
+    val settings = MongoClientSettings.builder()
+        .applyConnectionString(ConnectionString("mongodb://$mongoHost:$mongoPort"))
+        .applyToClusterSettings { it.hosts(listOf(ServerAddress(mongoHost, mongoPort))) }
+        .build()
+    parametersOf(settings)
+}
 
 val repository: CoinRepository by inject(CoinRepository::class.java) {
     parametersOf(coroutineClient)
